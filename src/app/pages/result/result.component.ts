4963f61d4cdc31e7c900ebc54cba90e72df5f809
Hartung, Michael <michael.hartung@uni-hamburg.de>
2022-07-20 11:44:39 +0200

    auto refresh of task status if not Done; result page tabs text dark

--- src/app/pages/result/result.component.ts
+++ src/app/pages/result/result.component.ts
@@ -30,16 +30,31 @@ export class ResultComponent implements OnInit {
   };
   public matrix: any;
 
+  public delay = (ms: any) => new Promise(res => setTimeout(res, ms));
+
   constructor(public taskService: TaskService, private route: ActivatedRoute, public resultService: ResultServiceService) { }
 
   ngOnInit(): void {
     this.route.params.subscribe(params => {
       this.key = params['key'];
-      this.taskService.getTask(this.key).then(response => {
-        this.taskData = response;
-        this.taskData.query.mail = this.taskData.query.mail ? this.taskData.query.mail : '';
-      });
+      this.getTask();
     })
+  }
+
+  public getTask() {
+    this.taskService.getTask(this.key).then(response => {
+      this.taskData = response;
+      this.taskData.query.mail = this.taskData.query.mail ? this.taskData.query.mail : '';
+      this.checkTaskStatus();
+    });
+  }
+
+  public async checkTaskStatus() {
+    if (this.taskData.status !== 'Done') {
+      console.log('updating task')
+      await this.delay(5000);
+      this.getTask(), 1
+    }
   }
 
 }
