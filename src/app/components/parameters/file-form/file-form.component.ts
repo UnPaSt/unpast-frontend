4252766fa9d06e1cc6c67dca4d6552ee28d046a3
Hartung, Michael <michael.hartung@uni-hamburg.de>
2025-10-12 16:03:01 +0200

    update package versions

--- src/app/components/parameters/file-form/file-form.component.ts
+++ src/app/components/parameters/file-form/file-form.component.ts
@@ -32,7 +32,7 @@ export class FileFormComponent implements OnInit {
         });
     }
 
-    private MAX_FILE_SIZE = 500; //MB
+    private MAX_FILE_SIZE = 200; //MB
 
     public fileTooLargeAlert = false;
     public fileTypeWrongAlert = false;
